7541f4053ac20fa900773c043485e6f064d23f4d
JUSTIN SOBIESKI <jsobieski@wccnet.edu>
2024-04-10 23:43:42 +0000

    Shuffle "questions"

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+import random
 
 app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).parent
@@ -16,11 +17,16 @@ async def index(request: Request):
     with open(BASE_DIR / "questions.json", "r") as file:
         quiz_data = json.load(file)
 
-    #print(quiz_data)
+    # Shuffle the options for each question
+    for question in quiz_data["quiz"]:
+        random.shuffle(question["options"])
+
     context = {
         "request": request,
         "title": "Multiple Choice Quiz",
         "quiz": quiz_data["quiz"]
     }
+
+    #print(quiz_data)
     response = templates.TemplateResponse("index.html", context)
     return response
