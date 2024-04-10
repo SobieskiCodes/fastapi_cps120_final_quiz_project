0eaf7ca5a314cf66c6a350d7f6b3486e590f306c
JUSTIN SOBIESKI <jsobieski@wccnet.edu>
2024-04-10 23:39:49 +0000

    dynamic questions.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
@@ -6,17 +7,20 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).parent
-templates = Jinja2Templates(directory=[
-    BASE_DIR / "templates",
-])
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
+    with open(BASE_DIR / "questions.json", "r") as file:
+        quiz_data = json.load(file)
+
+    #print(quiz_data)
     context = {
         "request": request,
-        "title": "Home Page"
+        "title": "Multiple Choice Quiz",
+        "quiz": quiz_data["quiz"]
     }
     response = templates.TemplateResponse("index.html", context)
     return response
