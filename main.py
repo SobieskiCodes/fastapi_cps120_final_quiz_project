abcd7378d35fc6d5431406e711661b326e80765a
JUSTIN SOBIESKI <jmksobieski@gmail.com>
2024-04-17 17:54:32 -0400

    Update main.py
    small tweaks / cleanup

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from random import shuffle
+import uvicorn
+
 
 app = FastAPI()
 BASE_DIR = pathlib.Path(__file__).parent
@@ -21,12 +23,12 @@ templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
+
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
     with open(BASE_DIR / "questions.json", "r") as question_file:
         quiz_data = json.load(question_file)
 
-    # Shuffle the options for each question
     for question in quiz_data["quiz"]:
         shuffle(question["options"])
 
@@ -36,7 +38,6 @@ async def index(request: Request):
         "quiz": quiz_data["quiz"]
     }
 
-    #print(quiz_data)
     response = templates.TemplateResponse("index.html", context)
     return response
 
@@ -51,11 +52,12 @@ async def submit_quiz(request: Request):
 
     score = 0
     for question in quiz_data["quiz"]:
-        #print('users answer:', user_answers.get(str(question["question"])), ', the answer:', question["answer"])
         if user_answers.get(str(question["question"])) == question["answer"]:
             score += 1
 
     result = f"You got {score} out of {len(quiz_data['quiz'])} questions right."
-    # i think i'd rather pop up or highlight the correct answers / questions vs a new page, but its a lot of work / javascript
-    #return templates.TemplateResponse("result.html", {"request": request, "result": result})
     return result
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
